2ec00d859f871183f7b625e3f99c09b7dbe8679c
 Алексадр Уренцев <alexandrurentsev@MacBook-Air-Aleksadr.local>
2024-02-04 17:58:37 +0300

    fix alembic init + migrate product

--- core/config.py
+++ core/config.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from pydantic_settings import BaseSettings
 
-from core.default_constants import DEFAULT_DB_URL
+from core.default_constants import DEFAULT_DB_URL, DEFAULT_DB_ECHO
 
 
 class Settings(BaseSettings):
     db_url: str = DEFAULT_DB_URL
+    db_echo: bool = DEFAULT_DB_ECHO
 
 
 settings = Settings()
@@ -16,4 +17,5 @@ app = FastAPI()
 async def info():
     return {
         "db_url": settings.db_url,
+        "db_echo": settings.db_echo,
     }
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from core.models.db_helper import db_helper
+from core.models.base import Base
+
 from products.router import router
 import uvicorn
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
 
 app = FastAPI()
 
