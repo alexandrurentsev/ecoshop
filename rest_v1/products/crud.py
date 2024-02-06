3785dbf3cfff60176f70999e2e97c0e76a1d15de
 Алексадр Уренцев <alexandrurentsev@MacBook-Air-Aleksadr.local>
2024-02-06 09:19:46 +0300

    fix

--- rest_v1/products/crud.py
+++ rest_v1/products/crud.py
@@ -1,18 +1,29 @@
 from typing import Union
-from sqlalchemy import select
-from sqlalchemy.orm import Session
-from sqlalchemy.ext.asyncio import AsyncSession
 
+from sqlalchemy import select
 
 from core.models.db_helper import async_session_maker
 from core.models.product import Product
+from rest_v1.products.schemas import ProductCreate
 
 
 class ProductRepository:
-    async def get_product(
-        product_id: int
-    ) -> Union[Product, None]:
+    @staticmethod
+    async def get_by_id(product_id: int) -> Union[Product, None]:
         async with async_session_maker() as session:
-            query = select(Product)
+            query = select(Product).filter(Product.id == product_id)
             result = await session.execute(query)
             return result.mappings().all()
+
+    @staticmethod
+    async def create_product(product_in: ProductCreate) -> Product:
+        async with async_session_maker() as session:
+            product = Product(
+                name=product_in.name,
+                description=product_in.description,
+                price=product_in.price,
+            )
+            session.add(product)
+            await session.commit()
+            await session.refresh(product)
+            return product
--- rest_v1/products/views.py
+++ rest_v1/products/views.py
@@ -1,10 +1,12 @@
+from typing import Union
 from fastapi import APIRouter
 
 from core.models.product import Product
 from rest_v1.products.crud import ProductRepository
+from rest_v1.products.schemas import ProductCreate
 
 
-router = APIRouter(tags=["Products"])
+router = APIRouter(tags=["Продукты"])
 
 product_repository = ProductRepository()
 
@@ -15,24 +17,14 @@ product_repository = ProductRepository()
 #     return await product_repository.get_all_products(session=session)
 
 
-# @router.post(
-#     "/",
-#     response_model=Product,
-#     status_code=status.HTTP_201_CREATED,
-# )
-# async def create_product(
-#     product_in: ProductCreate,
-#     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
-# ):
-#     return await product_repository.create_product(
-# session=session, product_in=product_in
-# )
+@router.post(
+    "/",
+    response_model=Product,
+)
+async def create_product(product_in: ProductCreate):
+    return await product_repository.create_product(product_in=product_in)
 
 
-@router.get("/{product_id}", response_model=Product)
-def get_product_by_id(product_id: int):
-    repository = ProductRepository()
-    return repository.get_by_id(product_id=product_id)
-
-
-
+@router.get("/{product_id}")
+async def get_product_by_id(product_id: int) -> Union[Product, None]:
+    return await product_repository.get_by_id(product_id=product_id)
