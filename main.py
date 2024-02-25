170486cad7b6bb63fa3a624abb95726d2a131c40
 Алексадр Уренцев <alexandrurentsev@Air-Aleksadr.lan>
2024-02-25 12:20:13 +0300

    feat CORSM

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 
@@ -16,6 +17,22 @@ app.include_router(rest_v1)
 app.include_router(router_pages)
 app.include_router(router_images)
 
+origins = [
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=[
+        "Content-Type" "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",  
+    ],
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
